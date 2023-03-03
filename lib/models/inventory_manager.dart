854455dea5e214725aa4adebd5792de796d8784c
Joao Gabriel <gabrielriery@gmail.com>
2023-03-03 01:22:37 -0500

    gridview consumables working

--- lib/models/inventory_manager.dart
+++ lib/models/inventory_manager.dart
@@ -1,7 +1,20 @@
+import 'package:dark_souls/api/mock_items_service.dart';
+import 'package:dark_souls/models/consumable.dart';
+import 'package:dark_souls/models/shield.dart';
+import 'package:dark_souls/models/speel.dart';
+import 'package:dark_souls/models/weapon.dart';
 import 'package:flutter/material.dart';
 
 //Here, will pass all the data towards menu equipament
 class InventoryManager extends ChangeNotifier {
+  final List<Consumable> consumables = [];
+  final List<Weapon> weapons = [];
+  final List<Shield> shields = [];
+  final List<Speel> speels = [];
+
+  final mockService = MockhItemsService();
+  bool isLoaded = false;
+  int selectedTab = 0;
   List<String> categoryImage = [
     "assets/items/category/consumable.png",
     "assets/items/category/weapons.png",
@@ -14,7 +27,23 @@ class InventoryManager extends ChangeNotifier {
     Colors.white,
   ];
 
-  int selectedTab = 0;
+  InventoryManager() {
+    _init();
+  }
+
+  void _init() async {
+    final consumablesList = await mockService.getConsumable();
+    final weaponsList = await mockService.getWeapon();
+    final shieldsList = await mockService.getShield();
+    final speelsList = await mockService.getSpeel();
+
+    consumables.addAll(consumablesList);
+    weapons.addAll(weaponsList);
+    shields.addAll(shieldsList);
+    speels.addAll(speelsList);
+    isLoaded = true;
+    notifyListeners();
+  }
 
   void goToTab(index) {
     selectedTab = index;
--- lib/view/inventory_items_view.dart
+++ lib/view/inventory_items_view.dart
@@ -1,3 +1,4 @@
+import 'package:dark_souls/components/item_tile.dart';
 import 'package:dark_souls/models/inventory_manager.dart';
 import 'package:dark_souls/view/menu_category_view.dart';
 import 'package:flutter/material.dart';
@@ -8,47 +9,75 @@ class InventoryItemsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<InventoryManager>(
-      builder: (context, value, child) {
-        return Scaffold(
-          backgroundColor: Colors.transparent,
-          body: LayoutBuilder(
-            builder: (contex, BoxConstraints bc) {
-              final size = bc.biggest;
-              return Column(
-                children: [
-                  //todo: Row of category
-                  SizedBox(
-                    height: size.height * 0.15,
-                    width: size.width,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: RowOfCategoryItems(),
-                    ),
+    List<Color> colors = [
+      Colors.red,
+      Colors.green,
+      Colors.black,
+      Colors.blue,
+      Colors.yellow,
+    ];
+    List<Color> colors2 = [
+      Colors.red,
+      Colors.black,
+      Colors.blue,
+      Colors.yellow,
+      Colors.green,
+    ];
+    final manager = Provider.of<InventoryManager>(context, listen: false);
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: LayoutBuilder(
+        builder: (contex, BoxConstraints bc) {
+          final size = bc.biggest;
+          return Column(
+            children: [
+              //todo: Row of category
+              SizedBox(
+                height: size.height * 0.15,
+                width: size.width,
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: RowOfCategoryItems(),
+                ),
+              ),
+              //todo: list of items of the selected category
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 5),
+                    itemCount: 4,
+                    itemBuilder: (context, index) {
+                      if (manager.isLoaded == true) {
+                        if (manager.selectedTab == 0) {
+                          return ItemTile(
+                              itemImage: manager.consumables[index].image);
+                        } else if (manager.selectedTab == 1) {
+                          return Container(
+                            color: colors2[index],
+                          );
+                        } else if (manager.selectedTab == 2) {
+                          return Container(
+                            color: colors[index],
+                          );
+                        } else if (manager.selectedTab == 3) {
+                          return Container(
+                            color: colors[index],
+                          );
+                        }
+                        return null;
+                      }
+                      return null;
+                    },
                   ),
-                  //todo: list of itens of the selected category
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ListView.builder(
-                        itemBuilder: (context, index) {
-                          if (value.selectedTab == 0) {
-                            return Container(
-                              color: Colors.white,
-                              height: 100,
-                            );
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
-        );
-      },
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
