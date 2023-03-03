d3606ce81fd55f7ec397341496f66152b0ae05fb
Joao Gabriel <gabrielriery@gmail.com>
2023-03-02 21:59:00 -0500

    category icons working
    ->add new list of images in InventoryManagement
    -> added 5 assets

--- lib/models/inventory_manager.dart
+++ lib/models/inventory_manager.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 //Here, will pass all the data towards menu equipament
 class InventoryManager extends ChangeNotifier {
+  List<String> categoryImage = [
+    "assets/items/category/consumable.png",
+    "assets/items/category/weapons.png",
+    "assets/items/category/shield.png",
+    "assets/items/category/speel.png"
+  ];
+
   List<Color> category = [
     Colors.black,
     Colors.white,
-    Colors.green,
-    Colors.blue,
-    Colors.red,
-    Colors.yellow
   ];
 
   int selectedTab = 0;
